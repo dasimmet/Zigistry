4589cefbfcee8227b7e2521f17d27b3750c46bbc
Tobias Simetsreiter <dasimmet@gmail.com>
2025-01-25 20:37:04 +0100

    stashed src/

---
+++ routes/api/programs/[user_name]/[...reponame].ts
@@ -0,0 +1,37 @@
+import programsMain from "../../../../../database/programs.json";
+import codebergMain from "../../../../../database/codebergPrograms.json";
+import gitlabMain from "../../../../../database/gitlabPrograms.json";
+
+const mainDatabase = [...programsMain, ...codebergMain, ...gitlabMain];
+
+export async function GET({
+  params,
+}: {
+  params: { user_name: string; reponame: string };
+}) {
+  const { user_name, reponame } = params;
+
+  // Validate that username and reponame are provided and are strings
+  if (!user_name || !reponame) {
+    return new Response(JSON.stringify(null), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const fullName = `${user_name.toLowerCase()}/${reponame.toLowerCase()}`;
+
+  // Search for the repository in the main database
+  const searchResults = mainDatabase.filter(
+    (item) => item.full_name.toLowerCase() === fullName,
+  );
+
+  return new Response(JSON.stringify(searchResults[0] || null), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
